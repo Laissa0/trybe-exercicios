842f6d80e898b8478478e11c4e493f036bdbf006
laissa0 <laissa.soares@geraestec.com.br>
2022-09-29 16:00:50 -0300

    Atividades da plataforma

---
+++ bloco-fundamentos/bloco-4-intro-javaScript/aula1.js
@@ -0,0 +1,12 @@
+let age = 20;
+age = 30;
+
+let name = "hulk"
+name = "fiona"
+
+const month = "janeiro"
+
+
+console.log(age);
+console.log(name);
+console.log(month);
---
+++ bloco-fundamentos/bloco-4-intro-javaScript/variaveis.js
@@ -0,0 +1,8 @@
+let myName = "Laissa";
+let birthCity = "Feira de Santana";
+birthCity = "Belo Horizonte";
+let birthYear = 2000;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
