c4f86f2d8a99ef40f8b8ddafc5caabca2882c9e7
laissa0 <laissa.soares@geraestec.com.br>
2022-09-29 16:21:00 -0300

    Atividade sobre tipos primitivos

---
+++ bloco-fundamentos/bloco-4-intro-javaScript/aula2.js
@@ -0,0 +1,25 @@
+let movie = "Avengers"; //string literal 
+
+let score = 10; // number literal 
+
+let IsValid = true; // boolean
+
+let name; // undefined 
+
+let color = null; // redefinicao de valor 
+
+<script src="scripts.js"></script> // forma de linkar com o JS "scripts " é o nome da pasta referente 
+
+// typeof identifica o tipo da variavel
+
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary * salary);
+console.log(salary / salary);
+console.log(salary ** salary);
+
+salary++; // incrementar 
+salary--; // decrementar
---
+++ bloco-fundamentos/bloco-4-intro-javaScript/tipos-primitivos.js
@@ -0,0 +1,21 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+
+
+let base = 5;
+let heigth = 8;
+let area = (base * heigth);
+let perimeter = (base ** base);
+
+console.log(base);
+console.log(heigth);
+console.log(area);
+console.log(base + heigth + base + heigth);
