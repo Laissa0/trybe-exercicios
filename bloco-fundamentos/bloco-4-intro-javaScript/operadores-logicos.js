2cbc0d498c99e967d98fd7aed8a5be56bcf8deb1
laissa0 <laissa.soares@geraestec.com.br>
2022-10-02 13:04:59 -0300

    exercicio operadores logicos

---
+++ bloco-fundamentos/bloco-4-intro-javaScript/operadores-logicos.js
@@ -0,0 +1,8 @@
+const comida = 'sushi';
+const bebida = 'coca-cola';
+
+if (bebida === 'coca-cola' && comida === 'pizza'){
+    console.log('Muito obrigado');
+} else {
+    console.log('Acho que está faltando algo');
+}
