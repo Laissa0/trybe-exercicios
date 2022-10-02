2b44a98519e25a1b57ea5552cc951f58957b2906
laissa0 <laissa.soares@geraestec.com.br>
2022-10-02 12:47:41 -0300

    atividade sobre condicional

---
+++ bloco-fundamentos/bloco-4-intro-javaScript/condicional.js
@@ -0,0 +1,12 @@
+
+let notaDesenvolvedora = 45;
+
+if (notaDesenvolvedora >= 80){
+    console.log("Parabens, voce foi aprovado!")
+}
+else if(notaDesenvolvedora < 80 && notaDesenvolvedora >= 60){
+    console.log("Voce esta na lista de espera")
+}
+else{
+    console.log("Voce foi reprovado");
+}
